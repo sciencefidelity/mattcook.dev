bc27e975a234c6a5630b6b4d0394357a1033dc66
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-01 01:49:06 +0000

    Add venn diagram

--- web/src/lib/Venn/index.svelte
+++ web/src/lib/Venn/index.svelte
@@ -15,7 +15,7 @@
 
 // ********* Venn Diagram *************
 
-@mixin circle($bottom: 10.02%, $left: 2.41%, $bg: c.$white, $blend: normal, $op: 1) {
+@mixin circle($bottom: 10.02%, $left: -1.91%, $bg: c.$white, $blend: normal, $op: 1) {
   position: absolute;
   width: 60.59%;
   border-radius: 50%;
@@ -27,50 +27,30 @@
   @extend %after;
 }
 
-@mixin content($top: 67%, $left: 50%) {
-  position: absolute;
-  transform: translate(-50%, -50%);
-  font-size: 5rem;
-  color: var(--light);
-  top: $top;
-  left: $left;
-}
-
 .venn {
   position: relative;
-  width: 90vh;
-  max-width: 88vw;
+  width: 81rem;
+  transform: rotate(-7deg);
+  padding-top: 7rem;
+  margin-left: -2rem;
   @extend %after;
   :nth-child(1) {
-    @include circle($bottom: 39.41%, $left: 19.705%);
+    @include circle($bottom: 37.41%, $left: 19.705%);
   }
   :nth-child(2) {
-    @include circle($left: 37%);
+    @include circle($left: 40.5%);
   }
   :nth-child(3) {
     @include circle();
   }
   :nth-child(4) {
-    @include circle($bottom: 39.41%, $left: 19.705%, $bg: c.$green, $blend: difference, $op: 0.65);
+    @include circle($bottom: 37.41%, $left: 19.705%, $bg: c.$green, $blend: difference, $op: 0.65);
   }
   :nth-child(5) {
-    @include circle($left: 37%, $bg: c.$red, $blend: difference, $op: 0.65);
+    @include circle($left: 40.5%, $bg: c.$red, $blend: difference, $op: 0.65);
   }
   :nth-child(6) {
     @include circle($bg: c.$cyan, $blend: difference, $op: 0.65);
-  }
-  // Labels
-  :nth-child(7) {
-    @include content($top: 18%, $left: 50%);
-  }
-  :nth-child(8) {
-    @include content($left: 21%);
-  }
-  :nth-child(9) {
-    @include content($left: 78%);
-  }
-  :nth-child(10) {
-    @include content($top: 50%);
   }
 }
 
