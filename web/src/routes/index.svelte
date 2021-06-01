d54501fb3d3f85d7b06e58212517cff126ded140
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-01 02:20:12 +0000

    Add email address, color logos

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -5,7 +5,7 @@
 <script lang="ts">
 import Venn from '$lib/Venn/index.svelte';
 import Logos from '$lib/Logos/index.svelte';
-import github from "../svg/github-brands.svg"
+import github from "./github-brands.svg"
 </script>
 
 <svelte:head>
@@ -20,7 +20,11 @@ import github from "../svg/github-brands.svg"
   <Venn/>
   <Logos/>
 
-  <a href="https://github.com/sciencefidelity" target="_blank" >
+  <div class="main__contact">
+    <a href="mailto:matt@sciencefidelity.co.uk" class="main__email">matt@sciencefidelity.co.uk</a>
+  </div>
+
+  <a href="https://github.com/sciencefidelity" target="_blank" rel="noreferrer">
     <span class="screen-reader-text">Visit me on Github</span>
     <img src={github} alt="github" class="github"/>
   </a>
@@ -44,6 +48,19 @@ import github from "../svg/github-brands.svg"
   &__punctuation {
     color: c.$pink;
   }
+  &__contact {
+    position: absolute;
+    top: 72%;
+    right: 11rem;
+    transform: rotate(4.5deg);
+  }
+  &__email {
+    font-family: t.$mono;
+    font-weight: 400;
+    font-size: 2.8rem;
+    text-decoration: none;
+    color: c.$blue;
+  }
 }
 
 .github {
@@ -51,6 +68,42 @@ import github from "../svg/github-brands.svg"
   width: 4.4rem;
   right: 4.5rem;
   bottom: 5rem;
+}
+
+// ********** accessibility **********
+
+// text meant only for screen readers
+.screen-reader-text {
+  border: 0;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute !important;
+  width: 1px;
+  word-wrap: normal !important;
+}
+
+.screen-reader-text:focus {
+  background-color: #f1f1f1;
+  border-radius: 3px;
+  box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.6);
+  clip: auto !important;
+  clip-path: none;
+  color: #21759b;
+  display: block;
+  font-size: 0.875rem;
+  font-weight: 700;
+  height: auto;
+  left: 5px;
+  line-height: normal;
+  padding: 15px 23px 14px;
+  text-decoration: none;
+  top: 5px;
+  width: auto;
+  z-index: 100000;
 }
 
 </style>
