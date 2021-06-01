5e5cd16a1d4c8f97346e8f8f8fd3afbaf6ed1b07
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-01 00:24:15 +0000

    Add and style title and headline

--- web/src/lib/Header/index.svelte
+++ web/src/lib/Header/index.svelte
@@ -2,8 +2,11 @@
 
 </script>
 
-<header>
-  <h1>Hi<span class="punctuation">!</span> I'm Matt<span class="punctuation">.</span></h1>
+<header class="header">
+  <h1 class="header__title">
+    Hi<span class="header__punctuation">!</span>
+    I'm Matt<span class="header__punctuation">.</span>
+  </h1>
 </header>
 
 <style lang="scss">
@@ -11,16 +14,26 @@
 @use "../../scss/colors" as c;
 @use "../../scss/breakpoints" as b;
 
-header {
-  height: 1.2rem;
+.header {
   background: c.$dark;
+  // overflow: hidden;
+  position: relative;
+  height: 12rem;
+  &__title {
+    position: absolute;
+    font-family: t.$mono;
+    font-weight: 700;
+    font-style: italic;
+    font-size: 9.6rem;
+    line-height: 0;
+    color: c.$blue;
+    padding: 0 0 0 5rem;
+    // margin-top: 3.5rem;
+    transform: translateY(-0.5rem) rotate(-4.5deg);
+  }
+  &__punctuation {
+    color: c.$pink;
+  }
 }
 
-h1 {
-  color: c.$blue;
-}
-
-.punctuation {
-  color: c.$pink;
-}
 </style>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -12,10 +12,26 @@
   <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@600&family=Space+Mono:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
 </svelte:head>
 
-<section>
-
+<section class="main">
+  <h2 class="main__headline">I'm a multi-media artist and digital creative<span class="main__punctuation">.</span></h2>
 </section>
 
-<style>
+<style lang="scss">
+@use "../scss/typography" as t;
+@use "../scss/colors" as c;
+@use "../scss/breakpoints" as b;
+
+.main {
+  &__headline {
+    padding: 10.6rem 0 0 10.6rem;
+    font-size: 4.5rem;
+    transform: rotate(4.5deg);
+    line-height: 0;
+    font-family: t.$sans;
+  }
+  &__punctuation {
+    color: c.$pink;
+  }
+}
 
 </style>
