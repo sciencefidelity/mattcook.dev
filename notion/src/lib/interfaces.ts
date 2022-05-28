ab341474caa67244629ad98119b5add05246d0ee
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-28 02:51:09 +0100

    feat: generate pages for each post written in Notion

---
+++ notion/src/lib/interfaces.ts
@@ -0,0 +1,28 @@
+export interface Excerpt {
+  annotations: {
+    bold: boolean
+    italic: boolean
+    strikethrough: boolean
+    underline: boolean
+    code: boolean
+    color: string
+  }
+  href: string
+  plain_text: string
+  text: {
+    content: string
+    link: string
+  }
+  type: string
+}
+
+export interface Post {
+  _id: string
+  author: string
+  excerpt: Excerpt[]
+  published: string
+  slug: string
+  status: string
+  tags: string[]
+  title: string[]
+}
