0ab3673c296d9a805fb54359f1df1477f93f1353
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-09 22:35:12 +0100

    refactor: play with methods for calling groq-js

--- notion/src/lib/interfaces.ts
+++ notion/src/lib/interfaces.ts
@@ -26,3 +26,16 @@ export interface Post {
   tags: string[]
   title: string[]
 }
+
+export type JSONValue =
+  | string
+  | number
+  | boolean
+  | JSONObject
+  | JSONArray;
+
+interface JSONObject {
+  [x: string]: JSONValue
+}
+
+interface JSONArray extends Array<JSONValue> { }
