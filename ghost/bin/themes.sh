0a4f3a4a9a4581f5631abffad7c744a0ee88a602
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-18 13:59:09 +0100

    fix: add create config back

--- ghost/bin/themes.sh
+++ ghost/bin/themes.sh
@@ -1,10 +1,10 @@
 themes=(
-  casper
-  lyra
+	casper
+	lyra
 )
 
 mkdir -p content/themes/
 for theme in "${themes[@]}"
 do
-  cp -Rf "node_modules/$theme" content/themes/$theme
+	cp -Rf "node_modules/$theme" content/themes/$theme
 done
