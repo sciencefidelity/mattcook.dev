6ac0126783a4f3e62a40c82afd460f485a826712
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-24 16:33:44 +0100

    refactor: add data for work section

--- portfolio/lib/data.ts
+++ portfolio/lib/data.ts
@@ -23,3 +23,27 @@ export const social = [
     url: "https://twitter.com/sciencefidelity",
   },
 ];
+
+export const projects = [
+  {
+    title: "Mid Wales Arts",
+    body: "Design and build of a multilingual static site for a vibrant arts and culture centre. Includes dynamic pages for news, events, workshops, exhibitions and artists that are updated  via a custom CMS used by non-technical content editors.",
+    url: "https://midwalesarts.org.uk",
+    repo: "https://github.com/sciencefidelity/midwalesarts.org.uk",
+    image: "mwa-home-desktop.jpg",
+  },
+  {
+    title: "National Arts & Education Network",
+    body: "Design and build of a multilingual static site for a vibrant arts and culture centre. Includes dynamic pages for news, events, workshops, exhibitions and artists that are updated  via a custom CMS used by non-technical content editors.",
+    url: "https://artsed.wales",
+    repo: "https://github.com/sciencefidelity/artsed.wales",
+    image: "naen-home-desktop.jpg",
+  },
+  {
+    title: "The Estate of Stefan Knapp",
+    body: "Design and build of a multilingual static site for a vibrant arts and culture centre. Includes dynamic pages for news, events, workshops, exhibitions and artists that are updated  via a custom CMS used by non-technical content editors.",
+    url: "https://stefanknapp.art",
+    repo: "https://github.com/sciencefidelity/stefanknapp.art",
+    image: "knapp-home-desktop-2.jpg",
+  },
+];
